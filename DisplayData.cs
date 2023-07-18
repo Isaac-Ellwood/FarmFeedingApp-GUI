377a9ec6a3d3ac6e14a3bff0a8ae145ce94fdab3
Isaac Ellwood <kingisaacellwood@gmail.com>
2023-07-19 09:43:16 +1200

    Adding chart

---
+++ DisplayData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FarmFeedingAppV2
+{
+    public partial class DisplayData : Form
+    {
+        Chart chart;
+
+        public DisplayData()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateChart()
+        {
+            chart.
+        }
+    }
+}
